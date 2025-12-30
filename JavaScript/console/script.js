1a0c8e8a89a9e67120dd871580460b6ae809b8b3
y-jh107 <jin99545@naver.com>
2025-12-30 12:16:13 +0900

    JavaScript first commit

---
+++ JavaScript/console/script.js
@@ -0,0 +1,20 @@
+console.log('안녕하세요!');
+console.log(123);
+console.log(false);
+var greeting = '안녕하세요.';
+console.log(greeting);
+console.log({a: "a", b: "b"});
+console.table({a: "a", b: "b"});
+
+console.error('Error!');
+console.warn('Warning!');
+
+console.time('Hello');
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.timeEnd('Hello');
+
+console.clear();
