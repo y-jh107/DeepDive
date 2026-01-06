913532efebe05490d669100b56edda1ffb6a3305
y-jh107 <jin99545@naver.com>
2026-01-06 16:00:55 +0900

    Async/Await

--- JavaScript/async/script.js
+++ JavaScript/async/script.js
@@ -18,15 +18,29 @@
 //         console.error(error);
 //     }) 
 
-fetch('http://jsonplaceholder.typicode.com/todos/1')
-    .then(response1 => response1.json())
-    .then(json => console.log(json))
-    .then(() => fetch('http://jsonplaceholder.typicode.com/todos/2'))
-    .then(response2 => response2.json())
-    .then(json => console.log(json))
-    .catch((error) => {
+// fetch('http://jsonplaceholder.typicode.com/todos/1')
+//     .then(response1 => response1.json())
+//     .then(json => console.log(json))
+//     .then(() => fetch('http://jsonplaceholder.typicode.com/todos/2'))
+//     .then(response2 => response2.json())
+//     .then(json => console.log(json))
+//     .catch((error) => {
+//         console.error(error);
+//     })
+//     .finally(() => {
+//         console.log('작업 끝!');
+//     })
+
+async function makeRequests() {
+    try {
+        const response1 = await fetch('http://jsonplaceholder.typicode.com/todos/1')
+        const jsonResponse1 = await response1.json();
+        console.log(jsonResponse1);
+    } catch (error) {
         console.error(error);
-    })
-    .finally(() => {
-        console.log('작업 끝!');
-    })
+    } finally {
+        console.log('---모든 작업 끝---');
+    }
+}
+
+makeRequests();
