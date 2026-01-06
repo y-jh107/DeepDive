d65f55aba54828d867571b3cd55537d273800b44
y-jh107 <jin99545@naver.com>
2026-01-06 16:21:16 +0900

    Symbol Examples

--- JavaScript/symbol/script.js
+++ JavaScript/symbol/script.js
@@ -31,10 +31,55 @@
 
 // console.log(carA);
 
-const sym1 = Symbol.for('sym1');
-const sym2 = Symbol.for('sym1');
+// const sym1 = Symbol.for('sym1');
+// const sym2 = Symbol.for('sym1');
 
 // console.log(sym1 === sym2);
 
-console.log(Symbol.keyFor(sym1));
-console.log(Symbol.keyFor(sym2));
+// console.log(Symbol.keyFor(sym1));
+// console.log(Symbol.keyFor(sym2));
+
+// const RED = Symbol('red');
+// const ORANGE = Symbol('orange');
+// const YELLOW = Symbol('yellow');
+// const BLUE = Symbol('blue');
+// const dog = 'blue';
+
+// function getImportantLevel(color) {
+//     switch(color) {
+//         case RED:
+//             return 'very important';
+//         case ORANGE:
+//             return 'important';
+//         case YELLOW:
+//             return 'little important';
+//         case BLUE:
+//             return 'not important';
+//         default:
+//             console.log(`${color} is not included`);
+//     }
+// }
+
+// console.log(getImportantLevel(BLUE));
+// console.log(getImportantLevel(dog));
+
+const length = Symbol('length');
+class Car {
+    constructor() {
+        this[length] = 0;
+    }
+
+    add(brand, name) {
+        this[brand] = name;
+        this[length]++;
+    }
+}
+
+let myCars = new Car();
+myCars.add('kia', 'morning');
+myCars.add('hyundai', 'tuscon');
+myCars.add('genesis', 'gv80');
+
+for (const car in myCars) {
+    console.log(car, myCars[car]);
+}
