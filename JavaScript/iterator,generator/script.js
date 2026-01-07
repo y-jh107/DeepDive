6019b7da1695ab464d4dafaff270509c6e5f878a
y-jh107 <jin99545@naver.com>
2026-01-07 11:45:00 +0900

    Generator Practice

--- JavaScript/iterator,generator/script.js
+++ JavaScript/iterator,generator/script.js
@@ -32,10 +32,54 @@
 //     console.log(n);
 // }
 
-const set = new Set([1, 2, 3, 4]);
-console.log('set', set);
-const map = new Map([['a', 1], ['b', 2]]);
-console.log('map', map);
+// const set = new Set([1, 2, 3, 4]);
+// console.log('set', set);
+// const map = new Map([['a', 1], ['b', 2]]);
+// console.log('map', map);
 
-console.log(set[Symbol.iterator]());
-console.log(map[Symbol.iterator]());
+// console.log(set[Symbol.iterator]());
+// console.log(map[Symbol.iterator]());
+
+// function* sayNumbers() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+// }
+
+// Generator 함수의 반환이 Generator
+// const number = sayNumbers();
+
+// console.log(number.next());
+// console.log(number.next());
+// console.log(number.next());
+// console.log(number.next());
+
+// function* generatorFunction() {
+//     yield 1;
+// }
+
+// const generator = generatorFunction();
+// // generator = generator[Symbol.iterator]();
+
+// function* createIds() {
+//     let index = 1;
+    
+//     while(true) {
+//         yield index++;
+//     }
+// }
+
+// const gen = createIds();
+
+// console.log(gen.next().value);
+// console.log(gen.return(10).value);
+
+function* generatorFunction() {
+    yield* [1, 2, 3];
+}
+
+const generator = generatorFunction();
+
+for (const number of generator) {
+    console.log(number);
+}
