12413bff26ff04b924d6ce5be90aaf9442dd7098
y-jh107 <jin99545@naver.com>
2026-01-14 16:53:49 +0900

    반복문

--- JavaScript2/section3/src/index.js
+++ JavaScript2/section3/src/index.js
@@ -346,3 +346,73 @@
 // console.log(arr[0]);
 // console.log(arr[1]);
 // console.log(arr[2]);
+
+// ----------- 배열
+
+// for (let i = 1; i < 6; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 5; i > 0; i--) {
+//   console.log(i);
+// }
+
+// let i = 1;
+// while (i < 6) {
+//   console.log(i);
+//   i++;
+// }
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// let person = {
+//   name: "홍길동",
+//   age: 25,
+//   height: 180,
+// };
+
+// let newArray = Object.keys(person);
+
+// for (let i = 0; i < newArray.length; i++) {
+//   let nowKey = newArray[i]; // key
+//   console.log(`key: ${nowKey}, value: ${person[nowKey]}`);
+// }
+
+// console.log(Object.keys(person));
+
+// let newArray = Object.values(person);
+
+// for (let i = 0; i < newArray.length; i++) {
+//   console.log(`value: ${newArray[i]}`);
+// }
+// console.log(Object.values(person));
+
+// let newArray = Object.entries(person);
+
+// for (let i = 0; i < newArray.length; i++) {
+//   console.log(`key: ${newArray[i][0]}, value: ${newArray[i][1]}`);
+// }
+
+// console.log(Object.entries(person));
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i of arr) {
+//     console.log(i);
+// }
+
+let person = {
+  name: "홍길동",
+  age: 25,
+  height: 180,
+};
+
+for (let key in person) {
+  console.log(`key: ${key}, value: ${person[key]}`);
+}
+
+// ------------ 반복문
