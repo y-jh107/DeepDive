fd28a46d735901fe4512dc7556bd6e336c43f320
y-jh107 <jin99545@naver.com>
2026-01-15 13:16:24 +0900

    배열과 객체 구조분해할당

---
+++ JavaScript2/section4/src/index.js
@@ -0,0 +1,49 @@
+// let colors = ["green", "blue", "purple"];
+
+// let [c1, c2, c3] = colors;
+
+// let c1, c2, c3, c4; // 선언 분리 할당
+// [c1, c2, c3, c4 = "yellow"] = ["green", "blue", "purple"];
+
+// console.log(c1);
+// console.log(c2);
+// console.log(c3);
+// console.log(c4);
+
+// let a = 10;
+// let b = 5;
+
+// tmp = a;
+// a = b;
+// b = tmp;
+
+// [a, b] = [b, a];
+
+// console.log(a, b);
+
+// let colors = {
+//   c1: "green",
+//   c2: "blue",
+//   c3: "purple",
+// };
+
+// let c1 = colors.c1;
+// let c2 = colors.c2;
+// let c3 = colors.c3;
+
+// let { c1, c2, c3 } = colors;
+
+// let color1 = colors.c1;
+// let color2 = colors.c2;
+// let color3 = colors.c3;
+
+// let { c1: color1, c2: color2, c3: color3 } = colors;
+
+// let { c1, c2, c3, c4 } = colors;
+
+// console.log(c1);
+// console.log(c2);
+// console.log(c3);
+// console.log(c4);
+
+// ----- 배열과 객체 구조분해할당
