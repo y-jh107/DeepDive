955567e10a8c735b9f95a525179c1b54df7ca24f
y-jh107 <jin99545@naver.com>
2026-01-15 16:23:00 +0900

    API 호출

--- JavaScript2/section4/src/index.js
+++ JavaScript2/section4/src/index.js
@@ -327,3 +327,21 @@
 // start();
 
 // ----- async와 await
+
+// const response = fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => console.log(res))
+//   .catch((error) => console.log(error));
+
+// console.log(response);
+
+const getData = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
