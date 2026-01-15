524bcbc80e587789ef2fe9e9fc2871c2f9c70063
y-jh107 <jin99545@naver.com>
2026-01-15 17:25:18 +0900

    DOM API-2

--- JavaScript2/section5/src/index.js
+++ JavaScript2/section5/src/index.js
@@ -27,5 +27,39 @@
 // const clockElement = document.getElementById("clock");
 // clockElement.textContent = "12:00";
 
-const clockElement = document.querySelector("div.date");
-clockElement.textContent = "03.29 수요일";
+// const clockElement = document.querySelector("div.date");
+// clockElement.textContent = "03.29 수요일";
+
+// ----- DOM API-1
+
+// const seasonElement = document.createElement("div");
+// seasonElement.classList.add("season");
+// seasonElement.textContent = "spring";
+
+// const seasonText = document.createTextNode("spring");
+// const todayInfoElement = document.querySelector("div.today-info");
+// todayInfoElement.appendChild(seasonElement);
+// seasonElement.appendChild(seasonText);
+
+// console.log(document.getElementsByTagName("div"));
+
+// const buttonElement = document.createElement("div");
+// buttonElement.classList.add("button");
+// buttonElement.textContent = "버튼";
+
+// buttonElement.style.backgroundColor = "gray";
+// buttonElement.style.color = "white";
+// buttonElement.style.width = "50px";
+// buttonElement.style.textAlign = "center";
+// buttonElement.style.cursor = "pointer";
+
+// const todayInfoElement = document.querySelector("div.today-info");
+// todayInfoElement.appendChild(buttonElement);
+
+// buttonElement.addEventListener("click", () => {
+//   window.alert("click");
+// });
+
+// console.log(document.getElementsByTagName("div"));
+
+// ------ DOM API-2
