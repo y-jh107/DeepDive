2a739923795f8f8e47063f1f1911a2550f1b9a03
y-jh107 <jin99545@naver.com>
2026-01-26 15:58:45 +0900

    TodoList 수정하기 구현

--- React2/section08/src/components/List.jsx
+++ React2/section08/src/components/List.jsx
@@ -2,7 +2,7 @@ import "./List.css";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
 
-const List = ({ todos }) => {
+const List = ({ todos, onUpdate }) => {
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
@@ -30,7 +30,7 @@ const List = ({ todos }) => {
       />
       <div className="todos-wrapper">
         {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} />;
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
         })}
       </div>
     </div>
--- React2/section08/src/components/TodoItem.jsx
+++ React2/section08/src/components/TodoItem.jsx
@@ -1,10 +1,21 @@
 import "./TodoItem.css";
+import { useRef } from "react";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+  const contentRef = useRef();
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+    if (!isDone) contentRef.current.classList.add("clear");
+    else contentRef.current.classList.remove("clear");
+  };
+
   return (
     <div className="TodoItem">
-      <input readOnly checked={isDone} type="checkbox" />
-      <div className="content">{content}</div>
+      <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
+      <div ref={contentRef} className="content">
+        {content}
+      </div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
     </div>
