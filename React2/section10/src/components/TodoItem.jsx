90c16512cb2fec34d50fe37f3b2964b7d48bdfe8
y-jh107 <jin99545@naver.com>
2026-01-26 18:41:49 +0900

    React.memo와 컴포넌트 렌더링 최적화

--- React2/section10/src/components/TodoItem.jsx
+++ React2/section10/src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import "./TodoItem.css";
-import { useRef } from "react";
+import { memo, useRef } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const contentRef = useRef();
@@ -26,4 +26,16 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+// 고차 컴포넌트 (HOC, Higher Order Component)
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라 Props가 바뀌었는지 안바뀌었는지 판단
+  // T -> Props 바뀌지 않음 -> 리렌더링 X
+  // F -> Props 바뀜 -> 리렌더링 O
+
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
