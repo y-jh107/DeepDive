48bcd6f9b6007d46d5309a0ac0f47223f443e79a
양진혁 <jin99545@gmail.com>
2026-02-10 12:02:35 +0900

    컴포넌트 스트리밍 적용하기

--- NEXT.js/section06/src/app/(with-searchbar)/page.tsx
+++ NEXT.js/section06/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,10 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
 // 1. auto: 기본값, 아무것도 강제하지 않음
 // 2. force-dynamic: 페이지를 강제로 Dynamic 페이지로 설정
@@ -10,6 +12,7 @@ import { BookData } from "@/types";
 // 4. error: 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" },
@@ -29,6 +32,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } },
@@ -52,11 +56,15 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다 ...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다 ...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
