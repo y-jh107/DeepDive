8f0fd33013f10d6bf45d59d940000873cf173924
양진혁 <jin99545@gmail.com>
2026-02-10 17:51:01 +0900

    클라이언트 컴포넌트에서의 서버액션

--- NEXT.js/section07/src/actions/create-review.action.tsx
+++ NEXT.js/section07/src/actions/create-review.action.tsx
@@ -1,14 +1,18 @@
 "use server";
 
+import { error } from "console";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
@@ -19,7 +23,10 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       },
     );
-    console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     // 1. 특정 주소에 해당하는 페이지만 재검증
     // revalidatePath(`/book/${bookId}`);
@@ -35,8 +42,15 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그 기준, 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
+
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다: ${err}`,
+    };
   }
 }
