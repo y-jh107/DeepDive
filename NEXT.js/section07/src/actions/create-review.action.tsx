ce9f43240f7e85abbcb0f5aa02d90da5fde5661d
양진혁 <jin99545@gmail.com>
2026-02-10 17:21:42 +0900

    다양한 재검증 방식 살펴보기

--- NEXT.js/section07/src/actions/create-review.action.tsx
+++ NEXT.js/section07/src/actions/create-review.action.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
@@ -20,7 +20,21 @@ export async function createReviewAction(formData: FormData) {
       },
     );
     console.log(response.status);
-    revalidatePath(`/book/${bookId}`);
+
+    // 1. 특정 주소에 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // 2. 특정 경로의 모든 동적 페이지 재검증
+    // revalidatePath("/book/[id]", "page");
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath("/(with-searchbar)", "layout");
+
+    // 4. 모든 데이터 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidateTag(`review-${bookId}`);
   } catch (err) {
     console.error(err);
     return;
--- NEXT.js/section07/src/app/book/[id]/page.tsx
+++ NEXT.js/section07/src/app/book/[id]/page.tsx
@@ -11,6 +11,7 @@ export function generateStaticParams() {
 async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } },
   );
 
   if (!response.ok) {
