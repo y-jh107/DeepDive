891976756d78abd5076fa54e7272bb503862f02f
양진혁 <jin99545@gmail.com>
2026-02-10 11:15:17 +0900

    클라이언트 라우터 캐시

--- NEXT.js/section05/src/app/(with-searchbar)/layout.tsx
+++ NEXT.js/section05/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,7 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* <div>{new Date().toLocaleString()}</div> */}
       <Suspense fallback={<div>Loading ...</div>}>
         <Searchbar />
       </Suspense>
