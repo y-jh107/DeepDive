b5affe7228d4ac6bc2ea734cfb4136528d9bf820
양진혁 <jin99545@gmail.com>
2026-02-12 12:40:10 +0900

    on/off 버튼 생성, 기능 추가

--- React-Test/react-test-app/src/App.js
+++ React-Test/react-test-app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
@@ -11,15 +12,26 @@ function App() {
         <div>
           <button
             data-testid="minus-button"
+            disabled={disabled}
             onClick={() => setCount((count) => count - 1)}
           >
             -
           </button>
           <button
             data-testid="plus-button"
+            disabled={disabled}
             onClick={() => setCount((count) => count + 1)}
           >
             +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
           </button>
         </div>
       </header>
