06e3187ee4f8f0275cb597c5a2eb48517a79ce1a
양진혁 <jin99545@gmail.com>
2026-02-12 12:24:29 +0900

    +,- 버튼 onClick 이벤트 추가

--- React-Test/react-test-app/src/App.js
+++ React-Test/react-test-app/src/App.js
@@ -2,15 +2,25 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h3 data-testid="counter">{counter}</h3>
+        <h3 data-testid="counter">{count}</h3>
         <div>
-          <button data-testid="minus-button">-</button>
-          <button data-testid="plus-button">+</button>
+          <button
+            data-testid="minus-button"
+            onClick={() => setCount((count) => count - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            +
+          </button>
         </div>
       </header>
     </div>
--- React-Test/react-test-app/src/App.test.js
+++ React-Test/react-test-app/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("the counter starts at 0", () => {
@@ -19,4 +19,20 @@ test("plus button has correct text", () => {
   render(<App />);
   const plusButtonElement = screen.getByTestId("plus-button");
   expect(plusButtonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("When the - button is pressed, the counter changes to -1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
 });
