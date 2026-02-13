f5348bb2817cc13eda15b2c8cb5b9b59ee24df3c
양진혁 <jin99545@gmail.com>
2026-02-13 09:27:08 +0900

    에러 발생 시 처리

--- React-Test/travel-item-app/src/pages/OrderPage/Type.js
+++ React-Test/travel-item-app/src/pages/OrderPage/Type.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Products from "./Products";
+import ErrorBanner from "components/ErrorBanner";
 
 function Type({ orderType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadItems(orderType);
@@ -14,9 +16,13 @@ function Type({ orderType }) {
       let response = await axios.get(`http://localhost:5000/${orderType}`);
       setItems(response.data);
     } catch (error) {
-      console.error(error);
+      setError(true);
     }
   };
+
+  if (error) {
+    return <ErrorBanner message="에러가 발생했습니다" />;
+  }
 
   const ItemComponents = orderType === "products" ? Products : null;
 
