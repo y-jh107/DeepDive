32473b14d69cc3dfb40540de5bcac495b082ca66
양진혁 <jin99545@gmail.com>
2026-02-13 10:07:38 +0900

    UI 작업, 서버 추가

--- React-Test/travel-item-app/src/pages/OrderPage/Type.js
+++ React-Test/travel-item-app/src/pages/OrderPage/Type.js
@@ -35,7 +35,21 @@ function Type({ orderType }) {
     />
   ));
 
-  return <div>{optionItems}</div>;
+  return (
+    <>
+      <h2>주문 종류</h2>
+      <p>하나의 가격</p>
+      <p>총 가격:</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: orderType === "options" && "column",
+        }}
+      >
+        {optionItems}
+      </div>
+    </>
+  );
 }
 
 export default Type;
