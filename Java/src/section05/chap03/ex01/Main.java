f26559c922a27a0850eff223f05565e27d4d1885
양진혁 <jin99545@gmail.com>
2026-02-23 18:01:01 +0900

    클래스(정적) 필드와 메서드

---
+++ Java/src/section05/chap03/ex01/Main.java
@@ -0,0 +1,21 @@
+package section05.chap03.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+        //  💡 클래스 필드와 메소드는 인스턴스를 생성하지 않고 사용
+        String ycBrand = YalcoChicken.brand;
+        String ycContact = YalcoChicken.contact();
+
+        // ⚠️ 인스턴스 메소드는 불가
+        //  String ycName = YalcoChicken.name;
+        //  String ycIntro = YalcoChicken.intro();
+
+        YalcoChicken store1 = new YalcoChicken(3, "판교");
+        String st1Intro = store1.intro();
+
+        //  인스턴스에서는 클래스의 필드와 메소드 사용 가능
+        //  ⚠️ 편의상 기능일 뿐, 권장하지 않음 (혼란 초래. IDE에서 자동완성 안 됨 주목)
+        String st1Brand = YalcoChicken.brand;
+        String st1Contact = YalcoChicken.contact();
+    }
+}
---
+++ Java/src/section05/chap03/ex01/YalcoChicken.java
@@ -0,0 +1,29 @@
+package section05.chap03.ex01;
+
+public class YalcoChicken {
+
+    //  ⭐️ 클래스/정적 필드와 메소드들 : 본사의 정보와 기능
+    //  인스턴스마다 따로 갖고 있을 필요가 없는 것들에 사용
+    static String brand = "얄코치킨";
+
+    static String contact() {
+        //  ⚠️ 정적 메소드에서는 인스턴스 프로퍼티 사용 불가
+        //  System.out.println(name);
+
+        return "%s입니다. 무엇을 도와드릴까요?".formatted(brand);
+    }
+
+    int no;
+    String name;
+
+    YalcoChicken(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    String intro() {
+        //  💡 인스턴스 메소드에서는 정적 프로퍼티 사용 가능
+        return "안녕하세요, %s %d호 %s점입니다."
+                .formatted(brand, no, name);
+    }
+}
