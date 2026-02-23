ec74d8e793e77745d9a8b66c5e28e04600e64ab6
양진혁 <jin99545@gmail.com>
2026-02-23 12:09:43 +0900

    주문 확인 페이지

--- React-Test/travel-item-app/src/App.test.js
+++ React-Test/travel-item-app/src/App.test.js
@@ -26,4 +26,32 @@ test("From order to order completion", async () => {
     name: "주문하기",
   });
   userEvent.click(orderButton);
+
+  // ======== 주문 확인 페이지 ========
+  const summaryHeading = screen.getByRole("heading", { name: "주문 확인" });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const productsHeading = screen.getByRole("heading", {
+    name: "여행 상품: 5000",
+  });
+  expect(productsHeading).toBeInTheDocument();
+
+  const optionsHeading = screen.getByRole("heading", {
+    name: "옵션: 500",
+  });
+  expect(optionsHeading).toBeInTheDocument();
+
+  expect(screen.getByText("2 America")).toBeInTheDocument();
+  expect(screen.getByText("3 England")).toBeInTheDocument();
+  expect(screen.getByText("Insurance")).toBeInTheDocument();
+
+  const confirmCheckbox = screen.getByRole("checkbox", {
+    name: "주문하려는 것을 확인하셨나요?",
+  });
+  userEvent.click(confirmCheckbox);
+
+  const confirmOrderButton = screen.getByRole("button", {
+    name: "주문 확인",
+  });
+  userEvent.click(confirmOrderButton);
 });
---
+++ React-Test/travel-item-app/src/pages/CompletePage/CompletePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CompletePage() {
+  return <div>CompletePage</div>;
+}
+
+export default CompletePage;
