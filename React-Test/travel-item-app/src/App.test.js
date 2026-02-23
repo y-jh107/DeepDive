678dcb7cea605daec6bc262dc9e15db62ed63eec
양진혁 <jin99545@gmail.com>
2026-02-23 12:47:06 +0900

    주문 완료 페이지

--- React-Test/travel-item-app/src/App.test.js
+++ React-Test/travel-item-app/src/App.test.js
@@ -54,4 +54,21 @@ test("From order to order completion", async () => {
     name: "주문 확인",
   });
   userEvent.click(confirmOrderButton);
+
+  // ======== 주문 완료 페이지 ========
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+
+  const completeHeader = await screen.findByRole("heading", {
+    name: "주문이 성공했습니다.",
+  });
+  expect(completeHeader).toBeInTheDocument();
+
+  const loadingDisappeared = screen.queryByText(/loading/i);
+  expect(loadingDisappeared).not.toBeInTheDocument();
+
+  const firstPageButton = screen.getByRole("button", {
+    name: "첫페이지로",
+  });
+  userEvent.click(firstPageButton);
 });
--- React-Test/travel-item-app/src/mocks/handlers.js
+++ React-Test/travel-item-app/src/mocks/handlers.js
@@ -27,4 +27,8 @@ export const handlers = [
       ]),
     );
   }),
+  rest.post("http://localhost:5000/order", (req, res, ctx) => {
+    let dummyData = [{ orderNumber: 213213123123, price: 2000 }];
+    return res(ctx.json(dummyData));
+  }),
 ];
