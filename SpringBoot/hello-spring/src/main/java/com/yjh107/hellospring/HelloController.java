180ab0129997da43547cfe7dc525715368d525a9
양진혁 <jin99545@gmail.com>
2026-02-27 16:45:42 +0900

    JSON 응답 만들기

--- SpringBoot/hello-spring/src/main/java/com/yjh107/hellospring/HelloController.java
+++ SpringBoot/hello-spring/src/main/java/com/yjh107/hellospring/HelloController.java
@@ -3,11 +3,26 @@ package com.yjh107.hellospring;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello Spring Boot!";
+    }
+
+    @GetMapping("/user")
+    public User getUser() {
+        User user = new User("Jinhyuk", 28);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUserList() {
+        User user1 = new User("Jinhyuk", 25);
+        User user2 = new User("Jinhyuk", 20);
+        return List.of(user1, user2);
     }
 }
